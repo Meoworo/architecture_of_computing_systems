3fb1c800bc71a83fd6bc33ae9167c4cc919d31d2
Meoworo <mikkose@icloud.com>
2022-09-14 21:20:47 +0300

    upd

---
+++ lab_1/lab_1.cpp
@@ -0,0 +1,113 @@
+﻿// 1_lab.cpp
+
+#include <iostream>
+#include <stdio.h>
+#include <omp.h>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+#ifndef _OPENMP
+	printf("omp not supported!\n");
+#else
+	std::cout << "number of processors available : " << omp_get_num_procs() << std::endl;
+	std::cout << "number of threads : " << omp_get_max_threads() << std::endl;
+
+	std::cout << "\n--task 3--\n\n";	//3
+#pragma omp parallel
+	{
+		std::cout << "sequential region 1\n";
+	}
+#pragma omp parallel num_threads(3)
+	{
+		std::cout << "\tsequential region 2\n";
+	}
+	omp_set_num_threads(4);
+#pragma omp parallel
+	{
+		std::cout << "\t\tsequential region 3\n";
+	}
+
+	std::cout << "\n--task 1 --\n\n";	//1
+	std::cout << "number of threads : " << omp_get_max_threads() << std::endl;
+	omp_set_num_threads(5);
+	std::cout << "set num threads 5" << std::endl;
+	std::cout << "number of threads : " << omp_get_max_threads() << std::endl << std::endl;
+
+	std::cout << "sequential area 1" << std::endl;
+#pragma omp parallel
+	{
+		std::cout << "sequential area\n";
+	}
+	std::cout << "sequential area 2" << std::endl;
+
+	std::cout << "\n--task 2--\n\n";	//2
+	std::cout << "number of threads : " << omp_get_max_threads() << std::endl;
+#pragma omp parallel
+	{
+		std::cout << "my number: " << omp_get_thread_num() << std::endl;
+	}
+
+	std::cout << "\n--task 4--\n\n";	//4
+	std::cout << "number of threads : " << omp_get_max_threads() << std::endl;
+	omp_set_num_threads(128);
+	std::cout << "set num threads 128" << std::endl;
+	std::cout << "number of threads : " << omp_get_max_threads() << std::endl << std::endl;
+	omp_set_dynamic(0);
+	std::cout << "(dynamic mode off) number of threads : " << omp_get_max_threads() << std::endl << std::endl;
+#pragma omp parallel
+	{
+		std::cout << "my number: " << omp_get_thread_num() << std::endl;
+	}
+	omp_set_dynamic(1);
+	std::cout << "(dynamic mode on ) number of threads : " << omp_get_max_threads() << std::endl << std::endl;
+#pragma omp parallel
+	{
+		std::cout << "my number: " << omp_get_thread_num() << std::endl;
+	}
+
+	std::cout << "\n--task 5--\n\n";	//5
+	std::cout << "enter argument: ";
+	std::string s;
+	std::cin >> s;
+	std::string par("parallel");
+	std::string ser("serial");
+	bool flag = false;
+	if (s != par && s != ser)
+		std::cout << "invalid argument\n";
+	else
+		if (s == par)
+		{
+			flag = true;
+			std::cout << "parallel mod on\n";
+		}
+		else
+			std::cout << "serial mod on\n";
+
+	std::cout << "\nnumber of threads : " << omp_get_max_threads() << std::endl;
+	omp_set_num_threads(5);
+	std::cout << "set num threads 5" << std::endl;
+	std::cout << "number of threads : " << omp_get_max_threads() << std::endl << std::endl;
+	int len = 10000000;
+	auto t = omp_get_wtime();;
+
+	if (!flag) {
+		for (int i = 0; i < len; i++)
+			auto a = sin(i);
+		t = omp_get_wtime() - t;
+		std::cout << "time: " << t << std::endl;
+	}
+	else {
+		len /= omp_get_max_threads();
+#pragma omp parallel
+		{
+			for (int i = 0; i < len; i++)
+				auto a = sin(i);
+		}
+		t = omp_get_wtime() - t;
+		std::cout << "time: " << t << std::endl;
+	}
+
+#endif
+	system("pause");
+	return 0;
+}
---
+++ lab_2/lab_2.cpp
@@ -0,0 +1,100 @@
+﻿// 2_lab.cpp
+
+#include <iostream>
+#include <stdio.h>
+#include <omp.h>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+#ifndef _OPENMP	//OMP off
+	printf("omp not supported!\n");
+#else	//OMP on
+	cout << "number of processors available : " << omp_get_num_procs() << endl;
+	cout << "number of threads : " << omp_get_max_threads() << endl;
+
+	cout << "\n-- task 1 --\n\n";	//1
+	std::cout << "enter num a: ";
+	float a, b;
+	cin >> a;
+	std::cout << "enter num b: ";
+	cin >> b;
+	int len = 10000000;
+	auto t = omp_get_wtime();;
+	for (int i = 0; i < len; i++)
+		auto c = a * b;
+	t = omp_get_wtime() - t;
+	std::cout << "time (single): " << t << std::endl;
+	t = omp_get_wtime();
+	len /= omp_get_max_threads();
+#pragma omp parallel
+	{
+		for (int i = 0; i < len; i++)
+			auto c = a * b;
+	}
+	t = omp_get_wtime() - t;
+	std::cout << "time (parallel): " << t << std::endl;
+
+	cout << "\n-- task 2 --\n\n";	//2
+	t = omp_get_wtime();
+#pragma omp parallel
+	{
+		//space
+	}
+	t = omp_get_wtime() - t;
+	cout << "time: " << t << std::endl;
+
+	cout << "\n-- task 3 --\n\n";	//3
+	omp_set_num_threads(3);
+	cout << "number of threads : " << omp_get_max_threads() << endl;
+	cout << "omp single\n";
+#pragma omp parallel 
+	{
+		cout << "start (" << omp_get_thread_num() << ")\n";
+#pragma omp single
+		{ cout << "one thread (" << omp_get_thread_num() << ")\n"; }
+		cout << "finish (" << omp_get_thread_num() << ")\n";
+	}
+	cout << "omp single nowait\n";
+#pragma omp parallel 
+	{
+		cout << "start (" << omp_get_thread_num() << ")\n";
+#pragma omp single nowait
+		{ cout << "one thread (" << omp_get_thread_num() << ")\n"; }
+		cout << "finish (" << omp_get_thread_num() << ")\n";
+	}
+	cout << "\n-- task 4 --\n\n";	//4
+	cout << "number of threads : " << omp_get_max_threads() << endl;
+#pragma omp parallel
+	{
+		cout << "(" << omp_get_thread_num() << ") beginning\n";
+#pragma omp master
+		{
+			cout << "(" << omp_get_thread_num() << ") MASTER\n";
+			cout << "(" << omp_get_thread_num() << ") MASTER\n";
+		}
+		cout << "(" << omp_get_thread_num() << ") middle\n";
+#pragma omp master
+		{
+			cout << "(" << omp_get_thread_num() << ") MASTER\n";
+			cout << "(" << omp_get_thread_num() << ") MASTER\n";
+		}
+		cout << "(" << omp_get_thread_num() << ") end\n";
+	}
+	cout << "\n-- task 5 --\n\n";	//5
+	cout << "number of threads : " << omp_get_max_threads() << endl;
+	omp_set_num_threads(2);
+	cout << "number of threads : " << omp_get_max_threads() << endl;
+	int n = 10;
+	cout << "n = " << n << endl;
+#pragma omp parallel private(n)
+	{
+		cout << ("test");
+
+	}
+
+
+#endif
+	system("pause");
+	return 0;
+}
